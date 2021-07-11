bf503a8a9e255aab929fca01d7acea80df548882
Tristan Budiman <trisbman>
2021-07-11 19:32:35 +0700

    Update: fixed `Last update` feature

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -4,12 +4,12 @@ fetch("https://api.github.com/repos/trisbman/aplikasi-rak-buku/branches/master")
     let date = data.commit.commit.committer.date;
     let lastUpdate = new Date(date).toString().split(" ");
     let now = new Date().toString().split(" ");
-    let isToday;
+    let isToday = 0;
     for (let i = 1; i <= 3; i++) {
-      isToday = lastUpdate[i] === now[i];
+      if (lastUpdate[i] === now[i]) isToday = 1;
     }
     lastUpdate = date.toString().split(/[TZ]/);
     lastUpdate[1] += " UTC";
     const span = document.getElementById("last-update");
-    span.innerText = lastUpdate[+isToday];
+    span.innerText = lastUpdate[isToday];
   });
